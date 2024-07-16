2b66117110f69c997ad234ec6845edd5896ee201
SLucaS1 <43004185+SLucaS1@users.noreply.github.com>
2024-07-16 23:34:08 +0200

    Update

---
+++ OPC_Server/Classi/clsStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace OPC_Server.Classi
+{
+    public class clsStatus
+    {
+        public string Status { get; set; } = "Iniz";
+
+        public string Address { get; set; } = "....";
+
+    }
+}
